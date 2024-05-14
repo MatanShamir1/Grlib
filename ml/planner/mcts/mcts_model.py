d31a5b1066054c88fd9de7563638484dc152b16a
matanshamir1998 <matan.shamir@live.biu.ac.il>
2024-05-15 00:13:57 +0300

    changing mcts to be closed-loop instead of open-loop

--- ml/planner/mcts/mcts_model.py
+++ ml/planner/mcts/mcts_model.py
@@ -19,14 +19,21 @@ class MonteCarloTreeSearch():
 		self.tree = tree
 		self.action_space = self.env.action_space.n
 		self.action_space = 3 # currently
-		state = self.env.reset()
-		self.tree.add_node(Node(action=None, action_space=self.action_space, reward=0, terminal=False))
+		state, _ = self.env.reset()
+		self.tree.add_node(Node(identifier=hash((None, None, None, tuple(env.unwrapped.agent_pos), state['direction'], 0)), state=state, action=None, action_space=self.action_space, reward=0, terminal=False, pos=env.unwrapped.agent_pos))
 
-	def expand(self, node):
+	def expand(self, node, depth):
 		action = node.untried_action()
 		state, reward, terminated, truncated, _ = self.env.step(action)
 		done = terminated | truncated
-		new_node = Node(action=action, action_space=self.action_space, reward=reward, terminal=done)
+		new_node = Node(identifier=hash((node.identifier, action, tuple(self.env.unwrapped.agent_pos), state['direction'], depth)), state=state, action=action, action_space=self.action_space, reward=reward, terminal=done, pos=self.env.unwrapped.agent_pos)
+		self.tree.add_node(new_node, node)
+		return new_node
+
+	def expand_selection_stochastic_node(self, node, resulting_identifier, terminated, truncated, reward, action, state):
+		# the new node could result in a terminating state.
+		done = terminated | truncated
+		new_node = Node(resulting_identifier, state=state, action=action, action_space=self.action_space, reward=reward, terminal=done, pos=self.env.unwrapped.agent_pos)
 		self.tree.add_node(new_node, node)
 		return new_node
 
@@ -44,44 +51,53 @@ class MonteCarloTreeSearch():
 		exploration_term = exploration_constant * sqrt(2 * log(parent.num_visits) / child.num_visits) # consider not multiplying by 2
 		return child.performance + exploration_term
 
-	def best_child(self, node, exploration_constant):
-		best_child = self.tree.children(node)[0]
-		best_value = self.compute_value(node, best_child, exploration_constant)
+	# return the best action from a node. the value of an action is the weighted sum of performance of all children that are associated with this action.
+	def best_action(self, node, exploration_constant):
+		tried_actions_values = {} # dictionary mapping actions to tuples of (cumulative number of visits of children, sum of (child performance * num of visits for child)) to compute the mean later
 		iter_children = iter(self.tree.children(node))
-		next(iter_children)
 		for child in iter_children:
 			value = self.compute_value(node, child, exploration_constant)
-			if value > best_value:
-				best_child = child
-				best_value = value
-		return best_child
+			tried_actions_values.setdefault(child.action, [0, 0]) # create if it doesn't exist
+			tried_actions_values[child.action][0] += child.num_visits # add the number of child visits
+			tried_actions_values[child.action][1] += value * child.num_visits # add the relative performance of this child
+		return max(tried_actions_values, key=lambda k: tried_actions_values[k][1] / tried_actions_values[k][0]) # return the key (action) with the highest average value
 
 	# only changes the environment to make sure the actions which are already a part of the plan have been executed.
 	def execute_partial_plan(self, plan):
-		for action in plan:
-			state, reward, terminated, truncated, _ = self.env.step(action)
-			done = terminated
-			if done: return False
-		return True
-
-	# finds the ultimate path from the root node to a terminal state (the one that maximized rewards)
-	def tree_policy(self):
 		node = self.tree.root
 		depth = 0
+		for action in plan:
+			depth += 1
+			# important to simulate the env to get to some state, as the nodes don't hold this information.	
+			state, reward, terminated, truncated, _ = self.env.step(action)
+			done = terminated
+			if done: return None, False
+			resulting_identifier = hash((node.identifier, action, tuple(self.env.unwrapped.agent_pos), state['direction'], depth))
+			node = self.tree.nodes[resulting_identifier]
+		return node, True
+
+	# finds the ultimate path from the root node to a terminal state (the one that maximized rewards)
+	def tree_policy(self, root_depth):
+		node = self.tree.root
+		depth = root_depth
 		while not node.terminal:
+			depth += 1
 			if self.tree.is_expandable(node):
-				# expansion
-				return self.expand(node), depth
+				# expansion - in case there's an action that never been tried, its value is infinity to encourage exploration of all children of a node.
+				return self.expand(node, depth), depth
 			else:
-				# selection
-				node = self.best_child(node, exploration_constant=1.0/sqrt(2.0))
+				# selection - balance exploration and exploitation, coming down the tree - but note the selection might lead to new nodes because of stochaticity.
+				best_action = self.best_action(node, exploration_constant=1.0/sqrt(2.0))
 				# important to simulate the env to get to some state, as the nodes don't hold this information.	
-				state, reward, terminated, truncated, _ = self.env.step(node.action)
-				# due to stochastity, nodes could sometimes be terminal and sometimes they aren't. important to update it.
-				depth += 1
-				done = terminated # no way it will be truncated if it is in the tree
-				# we only need this node to be terminal in some possible execution and we know the sequence of actions it represents could lead to a goal
-				node.terminal |= done	
+				state, reward, terminated, truncated, _ = self.env.step(best_action)
+				# due to stochasticity, nodes could sometimes be terminal and sometimes they aren't. important to update it. also, the resulting state
+				# could be a state we've never been at due to uncertainty of actions' outcomes.
+				# if the resulting state creates a parent-action-child triplet that hasn't been seen before, add to the tree and return it, similar result to 'expand'.
+				resulting_identifier = hash((node.identifier, best_action, tuple(self.env.unwrapped.agent_pos), state['direction'], depth))
+				if resulting_identifier not in node.children_identifiers:
+					return self.expand_selection_stochastic_node(node, resulting_identifier, terminated, truncated, reward, best_action, state)
+				# if got here, the resulting state's was already seen. we can proceed with the tree policy down the tree.
+				node = self.tree.nodes[resulting_identifier]
 		return node, depth
 
 	# receives a final state node and updates the rewards of all the nodes on the path to the root
@@ -91,30 +107,15 @@ class MonteCarloTreeSearch():
 			node.total_simulation_reward += value
 			node.performance = node.total_simulation_reward/node.num_visits
 			node = self.tree.parent(node)
-
-	# def forward(self):
-	#     self._forward(self.tree.root)
-
-	# def _forward(self,node):
-	#     best_child = self.best_child(node, exploration_constant=0)
-
-	#     print("****** {} ******".format(best_child.state))
-
-	#     for child in self.tree.children(best_child):
-	#         print("{}: {:0.4f}".format(child.state, child.performance))
-
-	#     if len(self.tree.children(best_child)) > 0:
-	#         self._forward(best_child)
 	
 	def generate_policy_sequence(self):
-		self.env.reset()
 		trace = []
 		node = self.tree.root
 		while len(self.tree.children(node)) > 0:
-			next = self.best_child(node, exploration_constant=0)
-			state, reward, terminated, truncated, _ = self.env.step(next.action) # take the action that lead to the resulting node.
-			trace.append(((state, self.env.unwrapped.agent_pos), next.action))
-			node = next
+			best_action = self.best_action(node, exploration_constant=0)
+			candidate_children = [child for child in self.tree.children(node) if child.action == best_action]
+			node = max(candidate_children, key=lambda node: node.num_visits)
+			trace.append(((node.state, node.pos), node.action))
 		return trace
 
 def save_model_and_generate_policy(tree, original_root, model_file_path, monteCarloTreeSearch):
@@ -134,41 +135,45 @@ def plan(env_name, problem_name):
 			return monteCarloTreeSearch.generate_policy_sequence()
 	if not os.path.exists(model_dir): # if we reached here, the model doesn't exist. make sure its folder exists.
 		os.makedirs(model_dir)
-	steps = 1000
+	steps = 500
 	print(f"No tree found. Executing MCTS, starting with {steps} rollouts for each action.")
-	random.seed(2)
 	env = gym.make(id=problem_name)
+	random.seed(2)	
 	tree = Tree()
 	monteCarloTreeSearch = MonteCarloTreeSearch(env=env, tree=tree)
 	original_root = tree.root
 	plan = [] # a sequence of actions
+	depth = 0
 	while not tree.root.terminal: # we iterate until the root is a terminal state, meaning the game is over.
 		max_reward = 0
-		depth = 0
 		iteration = 0
 		steps = int(steps*0.9)
 		print(f"Executing {steps} rollouts for each action now.")
-		tq = tqdm(range(steps), postfix=f"Iteration: {iteration}, Number of steps: {len(plan)}. Selection depth: {depth}. Maximum reward: {max_reward}")
+		tq = tqdm(range(steps), postfix=f"Iteration: {iteration}, Number of steps: {len(plan)}. Selection depth: {depth}. Maximum reward: {max_reward}")		
 		for n in tq:
 			iteration = n
 			env.reset()
 			# when executing the partial plan, it's possible the environment finished due to the stochasticity. the execution would return false if that happend.
-			if not monteCarloTreeSearch.execute_partial_plan(plan):
+			depth = len(plan)
+			tree.root = original_root # need to return it to the original root before executing the partial plan as it can lead to a different path and the root can change between iterations.
+			node, result = monteCarloTreeSearch.execute_partial_plan(plan)
+			if not result:
 				# false return value from partial plan execution means the plan is finished. we can mark our root as terminal and exit, happy with our plan.
 				tree.root.terminal = True
-				return save_model_and_generate_policy(tree=tree, original_root=original_root, model_file_path=model_file_path, monteCarloTreeSearch=monteCarloTreeSearch)	
-			node, depth = monteCarloTreeSearch.tree_policy() # find a path to a new unvisited node (unique sequence of actions) by utilizing explorative policy or choosing unvisited children recursively
+				return save_model_and_generate_policy(tree=tree, original_root=original_root, model_file_path=model_file_path, monteCarloTreeSearch=monteCarloTreeSearch)
+			tree.root = node # determine the roote to be the node executed after the plan for this iteration.
+			node, depth = monteCarloTreeSearch.tree_policy(root_depth=depth) # find a path to a new unvisited node (unique sequence of actions) by utilizing explorative policy or choosing unvisited children recursively
+			# if the node that returned from tree policy is terminal, the reward will be returned from "simulation" function immediately.
 			reward = monteCarloTreeSearch.simulation(node) # proceed from that node randomly and collect the final reward expected from it (heuristic)
 			if reward > max_reward:
 				max_reward = reward
 			monteCarloTreeSearch.backpropagation(node, reward)  # update the performances of nodes along the way until the root
 			tq.set_postfix_str(f"Iteration: {iteration}, Number of steps: {len(plan)}. Selection depth: {depth}. Maximum reward: {max_reward}")
 		# update the root and start from it next time.
-		tree.root = monteCarloTreeSearch.best_child(node=tree.root, exploration_constant=0)
-		print(f"Executed action {tree.root.action}")
-		plan.append(tree.root.action)
+		action = monteCarloTreeSearch.best_action(node=tree.root, exploration_constant=0)
+		plan.append(action)
+		print(f"Executed action {action}")
 	return save_model_and_generate_policy(tree=tree, original_root=original_root, model_file_path=model_file_path, monteCarloTreeSearch=monteCarloTreeSearch)	
-	
 	
 if __name__ == "__main__":
 	# register(
